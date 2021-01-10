56552de273b30d1338ea8b2770356c6c2e024cec
neoed <neoed@tlen.pl>
2021-01-10 13:54:27 +0100

    Initial commit

---
+++ src/Liczby.java
@@ -0,0 +1,14 @@
+public class Liczby {
+    public static void main(String[] args) {
+        int liczbaStart = 1234;
+        int liczbaEnd = 2345;
+        int dzielnik1 = 7;
+        int suma = 0;
+
+        for (int i = liczbaStart; i <= liczbaEnd; i++){
+          if (i % dzielnik1){
+
+          }
+        }
+    }
+}
